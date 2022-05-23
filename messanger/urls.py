a4a89c4e2fe4f35b9e236dbf5adff2f29e91900b
fomin2601 <fomin260145@gmail.com>
2022-05-24 01:23:49 +0300

    --Changes on room.html Added ajax_new_room and ajax_add_user_to_room for creating new rooms and adding new users to room

--- messanger/urls.py
+++ messanger/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:room_name>/', views.room, name='room'),
     path('ajax_login/', views.ajax_login, name='ajax_login'),
-    path('logout/', views.logout, name='logout')
+    path('logout/', views.logout, name='logout'),
+    path('ajax_new_room/', views.ajax_new_room, name='ajax_new_room'),
+    path('ajax_add_user_to_room/', views.ajax_add_user_to_room, name='ajax_add_user_to_room'),
 ]
